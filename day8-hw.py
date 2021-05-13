93df65498a20f6fd590c8d6d1fcbdd215d324618
boramkim0514 <83566064+boramkim0514@users.noreply.github.com>
2021-05-13 14:04:40 +0900

    Add files via upload

--- day8-hw.py
+++ day8-hw.py
@@ -6,7 +6,6 @@ c = con.cursor()
 c.execute('insert into Man values("김연아", 32)')
 c.execute('insert into Man values("손흥민", 30)')
 c.execute('insert into Man values("이강인", 21)')
-c.execute('select * from movie order by audience')
 c.execute('select * from Man')
 
 
