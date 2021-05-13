c13f0cbfad90ce65a680c0bdfa69988756d5023b
boramkim0514 <83566064+boramkim0514@users.noreply.github.com>
2021-05-13 14:03:44 +0900

    Add files via upload

---
+++ day8-hw.py
@@ -0,0 +1,18 @@
+import sqlite3 as s
+
+con = s.connect('c:\\dd\\mydb')
+c = con.cursor()
+
+c.execute('insert into Man values("김연아", 32)')
+c.execute('insert into Man values("손흥민", 30)')
+c.execute('insert into Man values("이강인", 21)')
+c.execute('select * from movie order by audience')
+c.execute('select * from Man')
+
+
+res = c.fetchall()
+
+print(res)
+
+con.commit()
+con.close()
